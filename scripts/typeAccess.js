e6db68dcde07deff1d21202ce1e9f168a8cf6eca
Luis Gustavo <gustavobarbosag7@gmail.com>
2021-10-12 19:56:21 -0300

    page login and landing

--- scripts/typeAccess.js
+++ scripts/typeAccess.js
@@ -8,3 +8,17 @@ inputsForm.forEach((input)=>{
     input.addEventListener('focus', (input)=>{input.srcElement.classList.toggle('active');});
     input.addEventListener('blur', (input)=>{input.srcElement.classList.toggle('active');});
 });
+
+// const buttonSingUp = document.querySelector('.signUp-action');
+// const main = document.querySelector('main');
+// const access = document.querySelector('.group-access');
+// const regis = document.querySelector('.group-registration');
+// const buttons = document.querySelectorAll('.button-form');
+
+// function initForm() {
+//     main.classList.toggle('registration');
+//     access.classList.toggle('off');
+//     regis.classList.toggle('on');
+// }
+
+// buttonSingUp.addEventListener('click', initForm);
